17e4856869d8092e4106b295544c26dfd7b9d2d7
softdevlpr <165158367+softdevlpr@users.noreply.github.com>
2026-03-26 14:21:57 +0530

    Change module export to ES6 syntax

--- src/routes/mlRoutes.js
+++ src/routes/mlRoutes.js
@@ -4,5 +4,4 @@ import { predictTests } from "../controllers/mlController.js";
 const router = express.Router();
 
 router.post("/predict-tests", predictTests);
-
-module.exports = router;
+export default router;
