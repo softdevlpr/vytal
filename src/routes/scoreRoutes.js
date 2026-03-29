12d8224b259931471b794fc121d7dc855c088026
Vinita Bhamboo <165158367+softdevlpr@users.noreply.github.com>
2026-03-30 00:40:29 +0530

    Delete src/routes/scoreRoutes.js

--- src/routes/scoreRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { saveSymptomLog } = require("../controllers/scoreController");
-
-router.post("/", saveSymptomLog);
-
-module.exports = router;
