877fedf01a6787435e1b0a025be274e23b6daa46
Vinita Bhamboo <165158367+softdevlpr@users.noreply.github.com>
2026-03-29 23:12:30 +0530

    Enhance prediction validation and logging

--- src/controllers/predictController.js
+++ src/controllers/predictController.js
@@ -1,35 +1,31 @@
 // controllers/predictController.js
 const axios = require("axios");
 
-// POST /api/predict
-// Forwards request to Flask ML service (port 5000)
 const predict = async (req, res) => {
   const { primary_symptom = "", answers = {} } = req.body || {};
 
-  // Validate input
-  if (Object.keys(answers).length < 6) {
+  if (!primary_symptom || Object.keys(answers).length < 6) {
     return res.status(400).json({
       success: false,
-      error: "6 answers required",
+      error: "primary_symptom and 6 answers required",
     });
   }
 
   try {
-    console.log("Sending to ML:", answers);
+    console.log("Sending to ML:", { primary_symptom, answers });
 
-    // Call Flask API
     const response = await axios.post(
       "http://127.0.0.1:8000/ml-predict",
-      { answers }
+      { primary_symptom, answers }
     );
 
     console.log("ML Response:", response.data);
 
-    // Send back to client
     res.json({
       success: true,
-      data: response.data,
+      data: response.data.data,
     });
+
   } catch (e) {
     console.error("ML ERROR:", e.response?.data || e.message);
 
