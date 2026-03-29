337419dab1f940b69f1acb913929df32d8412814
Shreyanshi Yadav <169088332+sshreyanshii@users.noreply.github.com>
2026-03-30 01:06:28 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
 const router = express.Router();
