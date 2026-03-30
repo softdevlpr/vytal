4322475f611f82e09487b55d5b8d82e0069e9697
Vinita Bhamboo <165158367+softdevlpr@users.noreply.github.com>
2026-03-30 22:51:54 +0530

    Update login_page.dart

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -95,8 +95,7 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-
-  // ✅ FIXED LOGIN FUNCTION
+  
   void _login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
@@ -108,7 +107,7 @@ class _LoginPageState extends State<LoginPage> {
 
     try {
       final response = await http.post(
-        Uri.parse("http://10.0.2.2:5000/api/auth/login"),
+        Uri.parse("http://10.0.2.2:3000/api/auth/login"),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode({
           "email": email,
