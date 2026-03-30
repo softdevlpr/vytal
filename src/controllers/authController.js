10040c6ab7f84d42e05ab9a140317330b0523812
Vinita Bhamboo <165158367+softdevlpr@users.noreply.github.com>
2026-03-30 22:40:04 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,11 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const isValidEmail = (email) => {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(email);
+};
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
@@ -15,7 +20,13 @@ const registerUser = async (req, res) => {
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
+    if (!isValidEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters" });
+    }
+    
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
@@ -30,13 +41,20 @@ const registerUser = async (req, res) => {
     });
 
     res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
       _id: user._id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
+      },
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      sucess: false,
+      message: error.message,
+    });
   }
 };
 
@@ -45,24 +63,39 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
+      
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+    if (!isValidEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(404).json({ message: "Invalid email or password" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password_hash);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     res.status(200).json({
+      sucess: true,
+      message: "login successful",
+      data:{
       _id: user._id,
       name: user.name,
       email: user.email,
       token: generateToken(user._id),
+      },
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      sucess:false,
+      message: error.message,
+    });
   }
 };
 
