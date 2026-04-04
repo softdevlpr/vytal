cc54854913fd84aa9beb9811cab2dd20010ebd1e
Vinita Bhamboo <165158367+softdevlpr@users.noreply.github.com>
2026-04-04 10:27:42 +0530

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,7 @@ import '../models/models.dart';
 
 class ApiService {
   static const String mlBaseUrl = 'http://10.0.2.2:8000';
-  static const String dbBaseUrl = 'https://vytal-backend-dxx8.onrender.com/api';
+  static const String dbBaseUrl = 'http://10.0.2.2:3000/api';
 
   static const _headers = {'Content-Type': 'application/json'};
 
